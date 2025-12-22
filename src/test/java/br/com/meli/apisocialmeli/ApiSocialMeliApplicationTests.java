0c099f825bd9cd2bdfc637fb5954a7652c1d01ec
AlissonFredo <douglasalissonsf@gmail.com>
2025-12-22 14:59:04 -0300

    first commit

---
+++ src/test/java/br/com/meli/apisocialmeli/ApiSocialMeliApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.meli.apisocialmeli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSocialMeliApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
